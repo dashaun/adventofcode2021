5444232966abdfb0d99a4ec97dd0eed44306e343
DaShaun <dashaun@dashaun.com>
2021-12-04 01:50:47 -0600

    Day 2 with test

---
+++ src/main/java/com/javagrunt/adventofcode2021/demo/Day2Part1.java
@@ -0,0 +1,44 @@
+package com.javagrunt.adventofcode2021.demo;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+@Component
+public class Day2Part1 implements InitializingBean {
+
+    private static final String FORWARD = "forward";
+    private static final String DOWN = "down";
+    private static final String UP = "up";
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(Objects.requireNonNull(
+                        this.getClass().getClassLoader()
+                                .getResourceAsStream("input.2.1.txt"))));
+
+        int horizontalPosition = 0;
+        int depth = 0;
+        String thisLine;
+        while ((thisLine = br.readLine()) != null) {
+            if(thisLine.startsWith(FORWARD)){
+                horizontalPosition = horizontalPosition + Integer.parseInt(thisLine.substring(FORWARD.length() + 1));
+            } else if (thisLine.startsWith(DOWN)){
+               depth = depth + Integer.parseInt(thisLine.substring(DOWN.length() + 1));
+            } else if (thisLine.startsWith(UP)){
+                depth = depth - Integer.parseInt(thisLine.substring(UP.length() + 1));
+            } else {
+                System.err.println("THIS SHOULD NOT HAPPEN");
+            }
+        }
+        System.out.println(this.getClass().getSimpleName() + " :: Horizontal Position " + horizontalPosition);
+        System.out.println(this.getClass().getSimpleName() + " :: Depth               " + depth);
+        int answer = horizontalPosition * depth;
+        System.out.println(this.getClass().getSimpleName() + " :: Final Answer        " + answer);
+    }
+}
---
+++ src/main/java/com/javagrunt/adventofcode2021/demo/Day2Part2.java
@@ -0,0 +1,48 @@
+package com.javagrunt.adventofcode2021.demo;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+@Component
+public class Day2Part2 implements InitializingBean {
+
+    private static final String FORWARD = "forward";
+    private static final String DOWN = "down";
+    private static final String UP = "up";
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(Objects.requireNonNull(
+                        this.getClass().getClassLoader()
+                                .getResourceAsStream("input.2.1.txt"))));
+
+        int horizontalPosition = 0;
+        int aim = 0;
+        int depth = 0;
+
+        String thisLine;
+        while ((thisLine = br.readLine()) != null) {
+            if(thisLine.startsWith(FORWARD)){
+                int forwardMove = Integer.parseInt(thisLine.substring(FORWARD.length() + 1));
+                horizontalPosition += forwardMove;
+                depth += (forwardMove * aim);
+            } else if (thisLine.startsWith(DOWN)){
+               aim += Integer.parseInt(thisLine.substring(DOWN.length() + 1));
+            } else if (thisLine.startsWith(UP)){
+                aim -= Integer.parseInt(thisLine.substring(UP.length() + 1));
+            } else {
+                System.err.println("THIS SHOULD NOT HAPPEN");
+            }
+        }
+        System.out.println(this.getClass().getSimpleName() + " :: Horizontal Position " + horizontalPosition);
+        System.out.println(this.getClass().getSimpleName() + " :: Depth               " + depth);
+        int answer = horizontalPosition * depth;
+        System.out.println(this.getClass().getSimpleName() + " :: Final Answer        " + answer);
+    }
+}
